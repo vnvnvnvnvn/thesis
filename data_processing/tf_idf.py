cdb9bdc22f94768b795004724ae5d0b2ca64537e
vnvnvnvnvn <16020258@vnu.edu.vn>
2020-12-11 19:56:31 +0800

    Update tf-idf plot gen

--- data_processing/tf_idf.py
+++ data_processing/tf_idf.py
@@ -1,15 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import argparse
 import networkx as nx
 import os
 import sys
 import pickle
+import matplotlib.pyplot as plt
 
-word_file = "word_file_x86"
 
-def idf(folder, max_blk_cnt):
-    wf = open(word_file)
-    words = wf.readlines()
+def plot_idf(word_count, df_count, num_doc, title):
+    wc_list = sorted(word_count.items(), key=lambda x: x[1])
+    wl, wc_num = list(zip(*wc_list))
+    plt.bar(range(len(wc_num)), wc_num, log=True)
+    plt.savefig(title+'_wc.png')
+
+    cnt = []
+    for w in wl:
+        cnt.append(df_count[w])
+    percentage = [1.0 * x / num_doc for x in cnt]
+    plt.bar(range(len(wc_num)), percentage, log=True)
+    plt.savefig(title+'_idf.png')
+
+
+def idf(folder, word_file, nested=False):
+    with open(word_file, 'r') as wf:
+        words = wf.readlines()
     words.sort()
     counter = {}
     word_count = {}
@@ -18,21 +33,16 @@ def idf(folder, max_blk_cnt):
         counter[w.strip()] = 0
         word_count[w.strip()] = 0
 
-    # files = []
-    # for subdir in os.listdir(folder):
-        # files += [os.path.join(folder, subdir, x) for x in os.listdir(os.path.join(folder, subdir))]
-    files = [os.path.join(folder, x) for x in os.listdir(folder)]
-    current_blk_cnt = 0
-    for i in range(len(files)):
-        # if current_blk_cnt >= max_blk_cnt:
-            # break
-        if i % 100 == 0:
-            print("Done with " + str(i))
-        if i >= len(files):
-            break
+    if nested:
+        files = []
+        for subdir in os.listdir(folder):
+            files += [os.path.join(folder, subdir, x) for x in os.listdir(os.path.join(folder, subdir))]
+    else:
+        files = [os.path.join(folder, x) for x in os.listdir(folder)]
+
+    for name in files:
         graph = nx.read_gpickle(files[i])
         for node in graph.nodes(data=True):
-            current_blk_cnt += 1
             distinct = set(node[1]['data'])
             if len(distinct) == 0:
                 continue
@@ -41,21 +51,22 @@ def idf(folder, max_blk_cnt):
                 counter[w.strip()] += 1
             for w in node[1]['data']:
                 word_count[w.strip()] += 1
-    # idf_data = sorted(counter.items(), key=lambda x: x[1])
-    # for d in idf_data:
-    #     print(d)
-    # word_data = sorted(word_count.items(), key=lambda x:x[1])
-    # for wd in word_data:
-    #     print(wd)
-    print(num_doc)
-    print(len(word_count))
-    with open('idf.pkl', 'wb') as idf_handle:
+    with open(word_file+'_'+str(num_doc)+'_idf.pkl', 'wb') as idf_handle:
         pickle.dump(counter, idf_handle)
-    with open('wc.pkl', 'wb') as wc_handle:
+    with open(word_file+'_'+str(num_doc)+'_wc.pkl', 'wb') as wc_handle:
         pickle.dump(word_count, wc_handle)
+    plot_idf(word_count, counter, num_doc, word_file)
+
 
 def main():
-    idf(sys.argv[1], int(sys.argv[2]))
+    parser = argparse.ArgumentParser(description="""Tinh IDF cho cac blocks""")
+    parser.add_argument('--folder', help='Folder simplified graph can xu ly', required=True)
+    parser.add_argument('--vocab', help='Vocab tuong ung voi folder', required=True)
+    parser.add_argument('--nested', action='store_true', help='Folder da cho co nested khong')
+    args = parser.parse_args()
+
+    idf(args.folder, args.vocab, args.nested is not None)
+
 
 if __name__=='__main__':
     main()
