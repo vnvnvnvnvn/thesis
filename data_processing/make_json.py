d06a7fe7dc2aa7f64355443c0d124f97b7abf762
vnvnvnvnvn <16020258@vnu.edu.vn>
2020-12-11 20:38:30 +0800

    Change make_json for demo

--- data_processing/make_json.py
+++ data_processing/make_json.py
@@ -10,8 +10,8 @@ import pickle
 from concurrent.futures import ProcessPoolExecutor
 
 def create_json(orig_path, root_json="benign_json_test", extension=""):
-    name = orig_path.replace("/", "_")
-    #name = os.path.basename(orig_path)
+    # name = orig_path.replace("/", "_")
+    name = os.path.basename(orig_path)
     json_path = os.path.join(root_json, name) + extension
     t = r2.open(orig_path, ['-2'])
     # jcontent = t.cmd('aaa;agfj @@f')
@@ -27,9 +27,6 @@ def create_json_virus(orig_folder, name, root_json="../new_jsons"):
     json_path = os.path.join(root_json, name)
     if os.path.isfile(json_path):
         return
-    maltype = name.split('/')[-2]
-    # if maltype != 'Virus':
-        # return
     t = r2.open(orig_path, ['-2'])
     jcontent = t.cmd('aaa;agfj @@f')
     t.quit()
@@ -40,11 +37,6 @@ def create_json_virus(orig_folder, name, root_json="../new_jsons"):
 def multi(func, args, workers):
     with ProcessPoolExecutor(workers) as ex:
         ex.map(func, args)
-    #pool = Pool(processes=workers, maxtasksperchild=10)
-    #pool.map(func, args)
-    #pool.terminate()
-    #pool.join()
-    #del pool
 
 def process_list(name):
     with open(name) as f:
@@ -52,13 +44,13 @@ def process_list(name):
         print(len(files))
         multi(create_json, files, 10)
 
-def process_folder(folder, t):
+def process_folder(folder):
     import random
     import time
     handle = open('reporting_type.pkl', 'rb')
     data_arch = pickle.load(handle)
     handle.close()
-    todo = [d for (d, arch) in data_arch.items() if arch == 'x86' and d.split('/')[-2]==t]
+    todo = [d for (d, arch) in data_arch.items() if arch == 'x86']
     random.seed(time)
     random.shuffle(todo)
     multi(partial(create_json_virus, folder), todo, 3)
@@ -68,7 +60,7 @@ if __name__=='__main__':
         print("USAGE:\n\tmake_json.py <folder>\n\tmake_json.py <list_file.txt>\n\tmake_json.py <executable_file>")
         exit()
     if os.path.isdir(sys.argv[1]):
-        process_folder(sys.argv[1], sys.argv[2])
+        process_folder(sys.argv[1])
         exit()
     if sys.argv[1][-4:] == '.txt':
         process_list(sys.argv[1])
